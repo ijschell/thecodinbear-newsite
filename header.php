e79ab4f1d99104f0f292f2a689c87a87917d0dee
Jonathan Schell <jschell.21.09.87@gmail.com>
2017-05-15 00:06:51 -0300

    remove video add favicon

--- header.php
+++ header.php
@@ -4,7 +4,17 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
+    <title>The Coding Bear</title>
+    <meta name="description" content="The Coding Bear | Desarrollo web y diseño gráfico para tu empresa o proyecto, llevar a cabo tus ideas es más fácil y económico de lo que tu piensas.">
+    <meta name="author" content="Jonathan Schell">
+    <meta name="keywords" content="Desarrollo web, Diseño gráfico, imagen empresarial, actualizaciónes webs, app, cursos online, The Coding Bear, freelances, Jonathan Schell">
+    <link rel="shortcut icon" href="./images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="./images/favicon.ico" type="image/x-icon">
+    <meta property="og:title" content="The Coding Bear" />
+    <meta property="og:url" content="http://www.thecodingbear.com/" />
+    <meta property="og:image" content="./fanpage/banner_cuadrado.png" />
+	   <meta property="og:description" content="The Coding Bear | Desarrollo web y diseño gráfico para tu empresa o proyecto, llevar a cabo tus ideas es más fácil y económico de lo que tu piensas." />
+    <meta name="ROBOTS" content="INDEX, FOLLOW">
     <link href="https://fonts.googleapis.com/css?family=Taviraj:200" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -12,5 +22,15 @@
     <link rel="stylesheet" href="./css/slick.css">
     <link rel="stylesheet" href="./css/slick-theme.css">
     <link rel="stylesheet" href="./css/master.css">
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-33862564-4', 'auto');
+      ga('send', 'pageview');
+
+    </script>
   </head>
   <body>
